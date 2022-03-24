7068d17d54d091c6ac1cbf7eddb03838df6bc294
bjy502 <bjy502@gmail.com>
2022-03-24 18:04:17 +0900

    6

---
+++ terraform/codebuild/backend.tf
@@ -0,0 +1,23 @@
+# S3 bucket for backend
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "tf-gachicoding-tfstate"
+}
+
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# DynamoDB for terraform state lock
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-lock"
+  hash_key       = "LockID"
+  billing_mode   = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
